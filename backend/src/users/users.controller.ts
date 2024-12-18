7db3bc3db0b57f0c6420171d9247da386ca5a8bc
daniaAhmed2005 <daniaadib.alhajjar2005@gmail.com>
2024-12-18 19:46:47 +0200

    Update users.controller.ts
    ,,

---
+++ backend/src/users/users.controller.ts
@@ -0,0 +1,38 @@
+import { Controller, Get, Post, Put, Delete, Param, Body } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { User } from '../models/users.schema';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  async findAll(): Promise<User[]> {
+    return this.usersService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string): Promise<User> {
+    return this.usersService.findOneById(id);
+  }
+
+  @Post()
+  async create(@Body() userData: Partial<User>): Promise<User> {
+    return this.usersService.create(userData);
+  }
+
+  @Put(':id')
+  async update(
+    @Param('id') id: string,
+    @Body() updateData: Partial<User>,
+  ): Promise<User> {
+    return this.usersService.update(id, updateData);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: string): Promise<void> {
+    return this.usersService.delete(id);
+  }
+}
+
+  //hi
